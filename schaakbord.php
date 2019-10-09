05f711cc69409bdca0652dabc7bab35c169f018c
Guillermo Latumahina <glatumahina@roc-dev.com>
2019-10-09 13:21:29 +0200

    Uitwerking van de exercise

--- schaakbord.php
+++ schaakbord.php
@@ -9,10 +9,10 @@
 
     <table width="270px" cellspacing="0px" cellpadding="0px" border="2px">
         <?php
-        for ($row = 1; $row <= 8; $row++) {
+        for ($i = 1; $i <= 8; $i++) {
             echo "<tr>";
             for ($col = 1; $col <= 8; $col++) {
-                $total = $row + $col;
+                $total = $i + $col;
                 if ($total % 2 == 0) {
                     echo "<td height=30px width=30px bgcolor=black></td>";
                 } else {
